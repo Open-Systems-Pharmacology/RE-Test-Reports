7f9ebb459ebfda001b3895e4159ebb5b1883ab0d
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2023-09-29 08:09:01 -0400

    Use check reporter

--- .github/workflows/test-reports.R
+++ .github/workflows/test-reports.R
@@ -4,4 +4,4 @@ library(ospsuite.reportingengine)
 # This line install the java script app that takes snapshots of reports
 webshot::install_phantomjs()
 
-test_dir(".github/workflows/tests", reporter = "summary", encoding = "utf-8")
+test_dir(".github/workflows/tests", reporter = "check", encoding = "utf-8")
