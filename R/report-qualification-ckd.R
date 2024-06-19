69ec799f827970090d6e5d65a845e0c30581dcda
pchelle <chelle.pierre@gmail.com>
2024-06-19 08:11:22 -0400

    Set up the qualification data and adapt template

--- R/report-qualification-ckd.R
+++ R/report-qualification-ckd.R
@@ -3,19 +3,86 @@
 #' Qualification Report for `Open-Systems-Pharmacology/Qualification-CKD` repository
 
 # Clean environent and (re-)load the ospsuite.reportingengine package
-rm(list=ls())
+rm(list = ls())
 library(ospsuite.reportingengine)
 
-# Load repository content
-download.file('https://github.com/Open-Systems-Pharmacology/Qualification-CKD/archive/refs/heads/main.zip', destfile = 'Qualification-CKD.zip')
-unzip('Qualification-CKD.zip', exdir = "Qualification-CKD")
-unlink('Qualification-CKD.zip')
+# Define input parameters for the worklfow
+workingDirectory <- "tests/Reports/Qualification-CKD"
+qualificationRunnerFolder <- "QualificationRunner"
+pkSimPortableFolder <- "PK-Sim"
+versionInfo <- QualificationVersionInfo$new("1.0", "11.0", "3.0")
 
-# Get `createQualificationReport()` template
-source(system.file("extdata", "qualification-workflow-template.R", package = "ospsuite.reportingengine"))
-createQualificationReport(
-   qualificationRunnerFolder = "QualificationRunner",
-   pkSimPortableFolder= "PK-Sim",
-   createWordReport = FALSE,
-   versionInfo = QualificationVersionInfo$new("1.0", "11.0", "3.0")
-   )
+# Load repository content and clean up downloads
+download.file("https://github.com/Open-Systems-Pharmacology/Qualification-CKD/archive/refs/heads/main.zip", destfile = "archive.zip")
+unzip("archive.zip", exdir = "archive")
+unlink("archive.zip")
+dir.create("Qualification-CKD")
+file.copy("archive/Qualification-CKD-main/Qualification/Input", workingDirectory, recursive = TRUE)
+unlink("archive", recursive = TRUE)
+
+#' @description Code hereafter is adapted from `createQualificationReport()` template
+qualificationPlanName <- "qualification_plan.json"
+qualificationPlanFile <- file.path(workingDirectory, "input", qualificationPlanName)
+
+#' The default outputs of qualification runner should be generated under `<workingDirectory>/re_input`
+reInputFolder <- file.path(workingDirectory, "re_input")
+#' The default outputs or RE should be generated under `<workingDirectory>/re_output`
+reOutputFolder <- file.path(workingDirectory, "re_output")
+
+#' Configuration Plan created from the Qualification Plan by the Qualification Runner
+configurationPlanName <- "report-configuration-plan"
+configurationPlanFile <- file.path(reInputFolder, paste0(configurationPlanName, ".json"))
+
+#' Set watermark that will appear in all generated plots
+#' Default is no watermark. `Label` objects from `tlf` package can be used to specify watermark font.
+watermark <- ""
+
+#' If not set, report created will be named `report.md` and located in the workflow folder namely `reOutputFolder`
+reportFolder <- file.path(workingDirectory, "report")
+reportPath <- file.path(reportFolder, "report.md")
+
+#' @description Start **Qualification Runner** to generate inputs for the reporting engine
+#' @param logFile If not `null` is passed internally via the `-l` option
+logFile <- NULL
+#' @param logLevel If not `null` is passed internally via the `--logLevel` option
+logLevel <- NULL
+#' @param overwrite If `true`, eventual results from the previous run of the QualiRunner/RE will be removed first
+overwrite <- TRUE
+
+startQualificationRunner(
+  qualificationRunnerFolder = qualificationRunnerFolder,
+  qualificationPlanFile = qualificationPlanFile,
+  outputFolder = reInputFolder,
+  pkSimPortableFolder = pkSimPortableFolder,
+  configurationPlanName = configurationPlanName,
+  overwrite = overwrite,
+  logFile = logFile,
+  logLevel = logLevel
+)
+
+#' @description Run Qualification Workflow to generate inputs for the reporting engine
+titlePageFile <- file.path(reInputFolder, "Intro/titlepage.md")
+addTitlePage <- all(
+  !is.null(versionInfo),
+  file.exists(titlePageFile)
+)
+if (addTitlePage) {
+  adjustTitlePage(titlePageFile, qualificationVersionInfo = versionInfo)
+}
+
+#' Load `QualificationWorkflow` object from configuration plan
+workflow <- loadQualificationWorkflow(
+  workflowFolder = reOutputFolder,
+  configurationPlanFile = configurationPlanFile
+)
+
+#' Set the name of the final report
+workflow$reportFilePath <- reportPath
+workflow$createWordReport <- createWordReport
+workflow$wordConversionTemplate <- wordConversionTemplate
+
+#' Set watermark. If set, it will appear in all generated plots
+workflow$setWatermark(watermark)
+
+#' Run the `QualificationWorklfow`
+workflow$runWorkflow()
