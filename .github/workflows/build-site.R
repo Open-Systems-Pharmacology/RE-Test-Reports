b0547d9802373cfcd060e39cce7ef135a6d9920b
pchelle <chelle.pierre@gmail.com>
2024-10-09 06:33:35 -0400

    Fixes #73 set up github action and add index to provide better home page

--- .github/workflows/build-site.R
+++ .github/workflows/build-site.R
@@ -1,11 +1,12 @@
-# R Script to be implemented through github action
-# and to be used to deploy webpages of the test reports
+#' @title build-site
+#' @description
+#' RScript to be used in github action
+#' Temporary builds vignettes and index for pkgdown to build wanted website
 
-# Step 1:
-# Creates the appropriate yaml configuration file for the pkgdown website
+#' @description
+#' Creates the appropriate yaml configuration file for the pkgdown website
 reportMapping <- jsonlite::fromJSON("tests/testthat/report-mapping.json")
 hasReference <- reportMapping$Reference
-
 websiteConfig <- c(
   "template:",
   "  bootstrap: 5",
@@ -44,9 +45,8 @@ websiteConfig <- c(
 )
 writeLines(websiteConfig, con = "_pkgdown.yml")
 
-
-# Step 2:
-# Prepare the articles folder to include the appropriate articles
+#' @description
+#' Prepare vignettes for articles section of the website
 dir.create("vignettes")
 for (reportFolder in reportMapping$Report) {
   file.copy(
@@ -56,11 +56,13 @@ for (reportFolder in reportMapping$Report) {
   )
 }
 
-# Initialize website
+#' @description
+#' Initialize website
+#' Since the vignettes were created, article sections are prepared accordingly
 pkgdown::init_site()
 
-# Step 3:
-# Build the articles
+#' @description
+#' Render reports as html articles for website
 for (reportFolder in reportMapping$Report) {
   file.copy(
     from = list.files(file.path("tests/Reports", reportFolder), full.names = TRUE),
@@ -71,10 +73,43 @@ for (reportFolder in reportMapping$Report) {
   pkgdown::build_article(reportFolder)
   unlink("vignettes/images", recursive = TRUE)
 }
+# This line could build an index.html for article
+# However, the navbar already covers it
 # pkgdown::build_articles_index()
 
-# Step 4:
-# Build home page
-# For home page it is possible to create index.md
-# That will provide shorter and more useful regarding the tests
+#' @description
+#' Build home page
+#' For home page, creates an index.md to provide only selectd information for website
+readmeContent <- readLines("README.md")
+toKeep <- grep(
+  pattern = "## Using Reports and Scripts as template",
+  readmeContent
+)
+# Insert in line 5 (after title, author and dates)
+# Some text for the website
+toInsert <- c(
+  "",
+  "## Welcome to the OSPSuite Reporting Engine test reports.",
+  "",
+  paste(
+    "This website aims at providing a simple and easy way",
+    "to look up available tested reports obtained from the OSPSuite Reporting Engine."
+  ),
+  "",
+  paste(
+    "The tested reports were split in the navigation bar above",
+    "between reports obtained from standard tests for Mean and Population Model Workflows,",
+    "and reports obtained from Qualification Workflows."
+  ),
+  "",
+  paste(
+    "More details on the test results and source R code are available in the",
+    "[RE-Test-Reports](https://github.com/Open-Systems-Pharmacology/RE-Test-Reports) Github Repo."
+  ),
+  "",
+  "## Results Overview",
+  ""
+)
+readmeContent <- c(readmeContent[1:5], toInsert, readmeContent[1:toKeep])
+writeLines(readmeContent, con = "index.md")
 pkgdown::build_home()
